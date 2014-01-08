6573fbca0ae191afb1cb33bf9be5abd8b98b33c7
Rome Portlock <rome@alivecor.com>
2014-01-07 16:21:26 -0800

    Add HL7 escaping

--- lib/simple_hl7/subcomponent.rb
+++ lib/simple_hl7/subcomponent.rb
@@ -1,7 +1,13 @@
 module SimpleHL7
   class Subcomponent < Struct.new(:value)
     def to_hl7(separator_chars)
-      value
+      hl7 = value
+      hl7.gsub!(separator_chars.escape, "\\E\\")
+      hl7.gsub!(separator_chars.field, "\\F\\")
+      hl7.gsub!(separator_chars.repetition, "\\R\\")
+      hl7.gsub!(separator_chars.component, "\\S\\")
+      hl7.gsub!(separator_chars.subcomponent, "\\T\\")
+      hl7
     end
 
     def to_s
--- spec/simple_hl7/subcomponent_spec.rb
+++ spec/simple_hl7/subcomponent_spec.rb
@@ -7,6 +7,17 @@ module SimpleHL7
         subf = Subcomponent.new('test')
         subf.to_hl7(SeparatorCharacters.defaults).should == 'test'
       end
+
+      it "escapes special characters in a string" do
+        subc = Subcomponent.new('peas&carrots')
+        subc.to_hl7(SeparatorCharacters.defaults).should == 'peas\T\carrots'
+      end
+
+      it "escapes all special characters" do
+        subc = Subcomponent.new('\\&^~|')
+        default_chars = SeparatorCharacters.defaults
+        subc.to_hl7(default_chars).should == "\\E\\\\T\\\\S\\\\R\\\\F\\"
+      end
     end
   end
 end
